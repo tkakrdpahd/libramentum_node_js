4fe97dbbf94e6ecde3a9b52282dd23196623fd75
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-02-12 12:42:52 -0500

    refactoring
    career detail page

--- public/refac.js
+++ public/refac.js
@@ -1,11 +1,9 @@
 function setLanguage(languageOnClick) {
-    // 언어 설정 저장
+
     localStorage.setItem("language", languageOnClick);
 
-    // 언어 버튼 스타일 업데이트
     updateLanguageButtonStyles();
 
-    // 언어 변경에 따른 페이지 컨텐츠 업데이트
     readAndWrite(sessionStorage.getItem("organization"), sessionStorage.getItem("idx"));
 }
 
@@ -30,8 +28,8 @@ function updateLanguageButtonStyles() {
 }
 
 function readAndWrite() {
-var idx = '001';
-var organization = 'agency';
+    var idx = '001';
+    var organization = 'agency';
     fetch("./refac.json") // JSON 파일 위치
         .then(response => response.json()) // 응답을 JSON으로 변환
         .then(json => {
@@ -41,14 +39,20 @@ var organization = 'agency';
             let contentFound = false; // 조건에 맞는 콘텐츠를 찾았는지 여부를 나타내는 플래그
             json[language].forEach(content => {
                 if (content.organization === organization && content.idx === idx) { // 조직과 idx가 매개변수와 일치하는 경우
-                    // 페이지 요소에 콘텐츠 삽입
+                    // 페이지 요소에 기본 콘텐츠 삽입
                     document.getElementById("idx").innerText = content.idx;
                     document.getElementById("organization").innerText = content.organization;
-                    document.getElementById("title").innerHTML = content.title;
+                    document.getElementById("title").innerText = content.title;
 
-                    element = document.createElement('h5'); // 'a' 태그 사용
-                    element.href = "#"; // href 속성 추가
-                    element.id = idx; // 생성된 요소에 id 설정
+                    // content 객체의 나머지 속성 순회
+                    Object.entries(content).forEach(([key, value]) => {
+                        if (!["idx", "organization", "title"].includes(key)) { // 기본 키를 제외한 속성에 대해 처리
+                            const element = document.createElement('h5');
+                            element.id = `${key}`; // 백틱(`)을 사용하여 문자열 템플릿을 적용
+                            element.innerText = value;
+                            document.querySelector('.career_detail').appendChild(element); // '.career_detail' 클래스를 가진 요소를 찾아 그 안에 새 요소를 추가
+                        }
+                    });
 
                     contentFound = true; // 조건에 맞는 콘텐츠를 찾음
                 }
